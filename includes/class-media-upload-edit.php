5794b0b2c4c6b61a6ab480bb052518444d071963
Andy (laptop) <andycharrington@gmail.com>
2011-10-06 22:34:58 +0100

    Media upload now works :-)

--- includes/class-media-upload-edit.php
+++ includes/class-media-upload-edit.php
@@ -9,14 +9,35 @@ class media_upload_edit{
 	}
 	
 	function __construct(){ 
+<<<<<<< HEAD
 		add_action( 'media_upload_tabs', array( &$this, 'edit_tabs' ) );
 		add_filter( 'attachment_fields_to_edit', array( &$this, 'attachment_fields_to_edit' ), 1, 2 );
+=======
+	
+		global $pagenow;
+
+		$referer = $_SERVER['HTTP_REFERER'];
+		$queries = explode('&', $referer);
+		
+		foreach($queries as $query){
+			$q = explode('=', $query);
+			if($q[0] == 'member_info_type'){
+				add_filter( 'attachment_fields_to_edit', array( &$this, 'attachment_fields_to_edit' ), 1, 2 );
+				break;
+			}
+		}
+		add_action( 'media_upload_tabs', array( &$this, 'edit_tabs' ) );
+>>>>>>> Media Uploader now works.
 
 	}	//function
 	
 	function edit_tabs($tabs){
 	
+<<<<<<< HEAD
 		if($_GET['member_info_type'] == 'image'){	
+=======
+		if($_GET['member_info_type'] == 'image' || $_GET['member_info_type'] == 'document'){	
+>>>>>>> Media Uploader now works.
 	
 			$tabs = array('');
 		
@@ -27,8 +48,11 @@ class media_upload_edit{
 	} // function
 	
 	function attachment_fields_to_edit($form_fields, $post){
+<<<<<<< HEAD
 	
 		//if($_GET['member_info_type'] == 'image'){
+=======
+>>>>>>> Media Uploader now works.
 		
 			echo 	'<script type="text/javascript">
 						jQuery(\'.savebutton, #media-upload-header, .howto, .post_title, .image_alt, .post_excerpt, .post_content, .url, .align, .image-size\').hide();
@@ -39,15 +63,24 @@ class media_upload_edit{
 	        $form_fields['buttons'] = array(
 	            'label' => '', 
 	            'value' => '',
+<<<<<<< HEAD
 	            'html' => "<input type='submit' class='button' name='send[$post->ID]' value='" . esc_attr__( 'Add image to product' ) . "' />",
+=======
+	            'html' => "<input type='submit' class='button' name='send[$post->ID]' value='" . esc_attr__( 'Add to profile' ) . "' />",
+>>>>>>> Media Uploader now works.
 	            'input' => 'html'
 	        );
 
 			return $form_fields;
+<<<<<<< HEAD
     
     	//}
 	
 	} // function	
+=======
+	
+	} // function		
+>>>>>>> Media Uploader now works.
 
 }
 
--- js/upload.js
+++ js/upload.js
@@ -3,28 +3,34 @@ jQuery(document).ready(function() {
 	jQuery('#mi_upload_image_button').click(function() {
 		window.send_to_editor = function(html) {
 			imgurl = jQuery('img',html).attr('src');
-			var current_images  = jQuery('#upload_image').val();
-			var images_array = current_images.split('~');
-			var i = 0;
-			jQuery.each(images_array, function() { 
-				i++;
-			});
-			var image_limit = jQuery('#image_limit').val();
-			if(i <= image_limit){
-				if(jQuery('#upload_image').val() == ''){
-					jQuery('#upload_image').val(imgurl);
-				}else{
-					jQuery('#upload_image').val(jQuery('#upload_image').val() + '~' + imgurl);
-				}
-				jQuery('#mi_images .mi_image:first').parent().append('<div><img class="mi_uploaded_img" src="' + imgurl + '" /><br><img style="cursor:pointer;" onClick="deleteImage(\'' +imgurl + '\');" src="' + MI.MI_url + '/img/delete.png" class="delete_image" /></div>');
-	 			jQuery('.mi_image:first').remove();
+			
+			if(jQuery('#upload_image').val() == ''){
+				jQuery('#upload_image').val(imgurl);
 			}else{
-				alert('You can not add any more images.');
+				jQuery('#upload_image').val(jQuery('#upload_image').val() + '~' + imgurl);
 			}
+			
+			jQuery('#mi_images .mi_image:first').parent().append('<div><img class="mi_uploaded_img" src="' + imgurl + '" /><br><img style="cursor:pointer;" onClick="deleteImage(\'' +imgurl + '\');" src="' + MI.MI_url + '/img/delete.png" class="delete_image" /></div>');
+ 			jQuery('.mi_image:first').remove();
 			tb_remove(); 
 		}
+<<<<<<< HEAD
 	 
 		tb_show('', MI.WPURL + 'media-upload.php?type=image&member_info_type=image&amp;TB_iframe=true');
+=======
+		var current_images  = jQuery('#upload_image').val();
+		var images_array = current_images.split('~');
+		var i = 1;
+		jQuery.each(images_array, function() { 
+			i++;
+		});		
+		var image_limit = jQuery('#image_limit').val();
+		if(i <= image_limit){
+			tb_show('', MI.WPURL + 'media-upload.php?type=image&member_info_type=image&TB_iframe=true');
+		}else{
+			alert('You can not add any more images.');
+		}
+>>>>>>> Media Uploader now works.
 		return false;
 	});
 	
@@ -32,28 +38,30 @@ jQuery(document).ready(function() {
 		window.send_to_editor = function(html) {
 			docurl = jQuery(html).attr('href');
 			docname = jQuery(html).html();
-			var current_documents  = jQuery('#upload_document').val();
-			var documents_array = current_documents.split('~');
-			var i = 0;
-			jQuery.each(documents_array, function() { 
-				i++;
-			});
-			var document_limit = jQuery('#document_limit').val();
-			if(i <= document_limit){
-				if(jQuery('#upload_document').val() == ''){
-					jQuery('#upload_document').val(docurl + '=' + docname);
-				}else{
-					jQuery('#upload_document').val(jQuery('#upload_document').val() + '~' + docurl + '=' + docname);
-				}
-				jQuery('#mi_documents .single_row:first').parent().append('<div class="single_row"><div class="mi_uploaded_doc" >' + docname + '</div><img style="cursor:pointer;" onClick="deleteDocument(\'' +docurl + '\', \'' + docname + '\');" src="' + MI.MI_url + '/img/delete.png" class="delete_document" /></div>');
-	 			jQuery('.mi_document:first').remove();
+
+			if(jQuery('#upload_document').val() == ''){
+				jQuery('#upload_document').val(docurl + '=' + docname);
 			}else{
-				alert('You can not add any more documents.');
+				jQuery('#upload_document').val(jQuery('#upload_document').val() + '~' + docurl + '=' + docname);
 			}
+			jQuery('#mi_documents .single_row:first').parent().append('<div class="single_row"><div class="mi_uploaded_doc" >' + docname + '</div><img style="cursor:pointer;" onClick="deleteDocument(\'' +docurl + '\', \'' + docname + '\');" src="' + MI.MI_url + '/img/delete.png" class="delete_document" /></div>');
+ 			jQuery('.mi_document:first').remove();
+
 			tb_remove(); 
 		}
 	 
-		tb_show('', MI.WPURL + 'media-upload.php?TB_iframe=true');
+		var current_documents  = jQuery('#upload_document').val();
+		var documents_array = current_documents.split('~');
+		var i = 1;
+		jQuery.each(documents_array, function() { 
+			i++;
+		});
+		var document_limit = jQuery('#document_limit').val();
+		if(i <= document_limit){	 
+			tb_show('', MI.WPURL + 'media-upload.php?type=image&member_info_type=image&TB_iframe=true');	
+		}else{
+			alert('You can not add any more documents.');
+		}		
 		return false;
 	});	
  
